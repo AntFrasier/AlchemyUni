9e7125e356f6c8af672b2c23ebef931bc8505348
AntFrasier <frasier@hotmail.fr>
2022-12-17 18:46:31 +0100

    done

--- client/index.js
+++ client/index.js
@@ -6,8 +6,16 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const myMerkleTree = new MerkleTree(niceList);
+
+  const root = myMerkleTree.getRoot();
+  const name = "Owen Kieh0n";
+  const index = niceList.findIndex( e => e == name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    proof : myMerkleTree.getProof(index),
+    name : name,
+    
     // TODO: add request body parameters here!
   });
 
