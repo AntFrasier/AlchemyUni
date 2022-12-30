c17924023c1101bcaf2d3d994b9947cd4807baa3
AntFrasier <frasier@hotmail.fr>
2022-12-30 19:36:25 +0100

    5 done !

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,19 +1,36 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const signers = await ethers.getSigners();
+    
+
+    return { game, signers};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signers } = await loadFixture(deployContractAndSetVariables);
+    const threshold = ethers.BigNumber.from("0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf") ;
+    let win = false;
+    let watchDog = 10000; // to avoid an infinite loop
+    while (!win && watchDog>0) {
+      let wallet =  ethers.Wallet.createRandom();
+      let signer = wallet.connect(ethers.provider);
+      await signers[0].sendTransaction({to: signer.address, value: ethers.utils.parseEther("0.3")}) //to signer to pay gas
+      let number = ethers.BigNumber.from(signer.address)
+      console.log( "Try : " ,signer.address)
+      if (number.lt(threshold) ) {
+        console.log(signer.address, " You Win")
+        await game.connect(signer).win();
+        win = true;
 
-    // good luck
-
-    await game.win();
+      }
+      watchDog--;
+    }
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
